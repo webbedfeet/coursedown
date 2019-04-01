3edaea857939fbdc297e0c8db9ddc0372c6ea16c
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-01 11:31:22 -0400

    updated process_slides to use render_site

--- R/process_slides.R
+++ R/process_slides.R
@@ -2,7 +2,4 @@ process_slides <- function(){
   require(rmarkdown)
   require(fs)
   render_site('slides')
-  if(dir_exists('docs/slides')) dir_delete('docs/slides')
-  dir_copy('slides/slides', 'docs')
-  dir_delete('slides/slides')
 }
