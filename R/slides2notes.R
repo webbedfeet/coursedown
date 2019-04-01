4ee2cc9554131684de9b61aad182187e3951611b
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-01 11:31:04 -0400

    Updated the slide2note function

--- R/slides2notes.R
+++ R/slides2notes.R
@@ -1,5 +1,25 @@
-slides2notes <- function(infile, outdir, outfile = infile){
-  knitr::purl(infile, output = file.path(outdir, outfile),
-              documentation = 1)
-  knitr::spin(file.path(outdir,outfile), knit = F, format='Rmd')
+slide2note <- function(infile, outdir = 'notes', outfile = basename(infile)){
+  require(stringr)
+  require(purrr)
+  require(magrittr)
+  require(rmarkdown)
+  require(yaml)
+  outfile <- file.path(outdir, outfile)
+  file_contents <- readLines(infile)
+  ind <- which(map_lgl(file_contents, str_detect,'---'))
+  title <- rmarkdown:::parse_yaml_front_matter(file_contents)$title
+  title <- paste('#',title)
+  bl <- file_contents[-(1:ind[2])] # remove frontmatter
+  bl <- bl %>%
+    `[`(!str_detect(., 'class')) %>%
+    `[`(!str_detect(., '---')) %>%
+    `[`(!str_detect(.,'layout')) %>%
+    `[`(!str_detect(., 'span')) %>%
+    str_replace('^#','##') %>%
+    c(c(title,''),.)
+  writeLines(bl, con=file(outfile))
+  yml <- read_yaml(file.path(outdir,'_bookdown.yml'))
+  yml$rmd_files$html <- unique(c(yml$rmd_files$html, basename(outfile)))
+  yml$rmd_files$latex <- unique(c(yml$rmd_files$latex, basename(outfile)))
+  write_yaml(yml, file.path(outdir,'_bookdown.yml'))
 }
