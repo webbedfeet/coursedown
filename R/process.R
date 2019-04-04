ee60e5458d86064f6fa8f7a5b8634fff7c582ce3
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-04 08:24:21 -0400

    Check if the docs folder exists

--- R/process.R
+++ R/process.R
@@ -8,6 +8,7 @@
 #' @examples
 process <- function(notes_pdf = FALSE){
   require(fs)
+  if(!file_exists('docs')) dir_create('docs')
   if(dir_exists('slides')) process_slides()
   if(dir_exists('notes')) process_notes(pdf = notes_pdf)
   if(dir_exists('assignments')) process_hw()
