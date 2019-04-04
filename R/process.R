a2deb5bb21381cb83ebaf541e89d122eb6927d9e
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-04 08:15:44 -0400

    improved processors

--- R/process.R
+++ R/process.R
@@ -11,4 +11,5 @@ process <- function(notes_pdf = FALSE){
   if(dir_exists('slides')) process_slides()
   if(dir_exists('notes')) process_notes(pdf = notes_pdf)
   if(dir_exists('assignments')) process_hw()
+  rmarkdown::render_site(here::here())
 }
--- R/process_slides.R
+++ R/process_slides.R
@@ -4,8 +4,18 @@
 #' @export
 #'
 #' @examples
-process_slides <- function(){
+process_slides <- function(pdf = FALSE){
   require(rmarkdown)
   require(fs)
-  render_site('slides')
+  if(pdf){
+    slnames <- dir_ls('slides', glob='*.Rmd') %>%
+      `[`(!str_detect('index'))
+    slnames <- path('slides', slnames)
+    map(slnames, ~try(slide2pdf(.)))
+    if(length(dir_ls('slides', glob = "*.pdf")) > 0){
+      file_copy(dir_ls('slides', glob='*.pdf'), 'docs/slides')
+    }
+  } else {
+    render_site('slides')
+  }
 }
