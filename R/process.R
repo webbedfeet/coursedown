24e60569094a7ab07157eaefa2ae304374d08990
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-05-02 04:30:08 -0400

    Updated process to use drake

--- R/process.R
+++ R/process.R
@@ -7,10 +7,19 @@
 #'
 #' @export
 #'
-process <- function(slides = T, notes = T, hw = T, notes_pdf = FALSE){
-  if(!fs::dir_exists(here::here('docs'))) fs::dir_create(here::here('docs'))
-  rmarkdown::render_site(here::here())
-  if(fs::dir_exists('slides') & slides) process_slides()
-  if(fs::dir_exists('notes') & notes) process_notes(pdf = notes_pdf)
-  if(fs::dir_exists('assignments') & hw) process_hw()
+process <- function(drake_source = here::here('_drake.R'),
+                    slides = T, notes = T, hw = T,
+                    slides_pdf = TRUE, notes_pdf = FALSE,
+                    lock_environment = TRUE){
+  requireNamespace('drake')
+  if(slides) process_slides(drake_source, pdf = slides_pdf, lock_environment = lock_environment)
+  if(hw) process_hw(drake_source)
+  if(notes) process_notes(drake_source, pdf = notes_pdf)
+  make(top_plan)
+  #
+  # if(!fs::dir_exists(here::here('docs'))) fs::dir_create(here::here('docs'))
+  # rmarkdown::render_site(here::here())
+  # if(fs::dir_exists('slides') & slides) process_slides()
+  # if(fs::dir_exists('notes') & notes) process_notes(pdf = notes_pdf)
+  # if(fs::dir_exists('assignments') & hw) process_hw()
 }
