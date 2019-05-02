e82865187a9238d416723c8c850a7fc84596e94c
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-05-02 03:37:23 -0400

    use drake for slides

--- R/process_slides.R
+++ R/process_slides.R
@@ -4,26 +4,35 @@
 #'
 #' @export
 #'
-process_slides <- function(pdf = FALSE){
+process_slides <- function(drake_source = here::here('_drake.R'), pdf = FALSE){
   requireNamespace("tidyverse")
-  final_output_dir <- fs::path(here::here('docs/slides'))
-  if(!fs::dir_exists(final_output_dir)) fs::dir_create(final_output_dir, recursive = TRUE)
-  yml <- yaml::read_yaml(fs::path(here::here('slides'),'_site.yml'))
-  if(yml$output_dir != fs::path_abs(fs::path(here::here('slides'), yml$output_dir))){
-    output_dir <- fs::path_abs(fs::path(here::here('slides'), yml$output_dir))
-  } else {
-    output_dir <- yml$output_dir
-  }
-  yml$output_dir <- fs::path_rel(output_dir, start = here::here('slides'))
-  yaml::write_yaml(yml, fs::path(here::here('slides'), '_site.yml'))
-  if(!fs::dir_exists(output_dir)) fs::dir_create(output_dir, recursive = TRUE)
-  rmarkdown::render_site('slides')
-  source('slides/lectures/drake.R')
-  make(plan)
-  if(pdf){
-    make(plan_pdf)
-  }
-  if (output_dir != final_output_dir){
-    fs::file_copy(fs::dir_ls(output_dir), final_output_dir, overwrite = TRUE)
-  }
+  requireNamespace('here')
+  requireNamespace('fs')
+  source(drake_source)
+  make(slides_plan)
+  #
+  # final_output_dir <- fs::path(here::here('docs/slides'))
+  # if(!fs::dir_exists(final_output_dir)) fs::dir_create(final_output_dir, recursive = TRUE)
+  # yml <- yaml::read_yaml(fs::path(here::here('slides'),'_site.yml'))
+  # if(yml$output_dir != fs::path_abs(fs::path(here::here('slides'), yml$output_dir))){
+  #   output_dir <- fs::path_abs(fs::path(here::here('slides'), yml$output_dir))
+  # } else {
+  #   output_dir <- yml$output_dir
+  # }
+  # yml$output_dir <- fs::path_rel(output_dir, start = here::here('slides'))
+  # yaml::write_yaml(yml, fs::path(here::here('slides'), '_site.yml'))
+  # if(!fs::dir_exists(output_dir)) fs::dir_create(output_dir, recursive = TRUE)
+  # rmarkdown::render_site('slides')
+  # setwd(here::here('slides','lectures'))
+  # system('make html')
+  # # source('slides/lectures/drake.R')
+  # # make(plan)
+  # if(pdf){
+  #   # make(plan_pdf)
+  #   system('make pdf')
+  # }
+  # setwd(here::here())
+  # if (output_dir != final_output_dir){
+  #   fs::file_copy(fs::dir_ls(output_dir), final_output_dir, overwrite = TRUE)
+  # }
 }
