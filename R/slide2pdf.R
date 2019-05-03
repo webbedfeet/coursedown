6b3b222f7f2f2d22ebed47d179b61285393e9156
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-05-02 22:48:27 -0400

    Converting xaringan to PDF

--- R/slide2pdf.R
+++ R/slide2pdf.R
@@ -1,9 +1,16 @@
-#' Converting slides to PDF format using chrome_print
+#' Converting xaringan slides to PDF
 #'
-#' @param slname Either the name of the HTML slides (preferred) or the name of the Rmd file
+#' @param f The relative path to the R Markdown file for the xaringan presentation
 #'
+#' @return The function produces a PDF in the folder where the HTML version of the file resides for web deployment
 #' @export
 #'
-slide2pdf <- function(slname){
-  pagedown::chrome_print(slname)
+#' @examples
+#' # slide2pdf('slides/lectures/00-Introduction.Rmd')
+slide2pdf <- function(f){
+  requireNamespace('stringr')
+  outdir <- here::here('docs','slides','lectures')
+  fname <- fs::path_file(f)
+  pdfname <- stringr::str_replace(fname, 'Rmd','pdf')
+  pagedown::chrome_print(f, output = fs::path(outdir, pdfname))
 }
