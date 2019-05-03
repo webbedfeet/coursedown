73d8aaf66f0b199df799c03c9b73e0a0dea138d1
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-05-02 22:48:12 -0400

    Compute the dates based on start date and week of course

--- R/classDate.R
+++ R/classDate.R
@@ -1,12 +1,19 @@
-#' Fill in a sequence of dates for class
+#' Compute the dates for the class, based on weekly classes
 #'
-#' @param wk The week number (integer >= 1)
+#' @param wk The week number
+#' @param startDate The first date for class, formatted as YYYY-MM-DD
+#' @param out_format The format in which the dates will be printed. Follows the conventions in \code{strptime}
 #'
-#' @return New dates
+#' @return A formatted date
 #' @export
 #'
 #' @examples
-classDate <- function(wk, startDate){
+#' classDate(3, '2019-01-28')
+classDate <- function(wk, startDate, out_format = "%B %d, %Y"){
+  if(!stringr::str_detect(as.character(startDate), '\\d{4}-\\d{2}-\\d{2}')){
+    stop('The start date must be formated as YYYY-MM-DD')
+  }
+  startDate <- as.Date(startDate)
   today <- startDate + 7*(wk-1)
   format(today, "%B %d, %Y")
 }
